d440e835952f526df83d58bb85de69f9f316acca
fabiopamaral <fabio.pamaral@outlook.com>
2023-06-07 16:35:06 -0300

    Entrada de dados pelo usuário

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class LivrariaApplication{
 
         public static void main(String[] args) {
             System.out.println("Olá, mundo!");
+
+            Scanner sc = new Scanner(System.in);
+             
+            System.out.println("Digite seu nome: ");
+            String nome = sc.nextLine();
+
+
+            sc.close();
+
+            System.out.println("Olá, " + nome + "!");
+
+
         }
     
 }
