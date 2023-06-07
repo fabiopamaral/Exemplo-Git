ffcb495255e9d3be7b973e5542d6e55e4ada58f4
fabiopamaral <fabio.pamaral@outlook.com>
2023-06-07 11:49:22 -0300

    Entrada do condicional

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class LivrariaApplication{
 
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        sc.close();
+
+        System.out.println("Olá, " + nome + "!");
+
+        if(idade >=18) {
+            System.out.println("Você é maior de idade!");
+        } else {
+            System.out.println("Você não é maior de idade!");
+        }
     }
     
 }
