491ea9bf452b4e0c651539c29b19e3959cf52f5f
fabiopamaral <fabio.pamaral@outlook.com>
2023-06-07 16:01:13 -0300

    Criação da classe Principal

--- LivrariaApplication.java
+++ LivrariaApplication.java
@@ -1,21 +1,8 @@
-import java.util.Scanner;
 
 public class LivrariaApplication{
 
-    public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite seu nome: ");
-        String nome = sc.nextLine();
-
-        System.out.println("Digite sua idade: ");
-        int idade = sc.nextInt();
-
-        sc.close();
-
-      
-    }
+        public static void main(String[] args) {
+            System.out.println("Olá, mundo!");
+        }
     
 }
